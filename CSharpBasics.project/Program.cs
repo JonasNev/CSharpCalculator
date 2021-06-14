6d10c561af2462b3f446581c6afe3bb2facb52d1
JonasNev <J.nevinskas@gmail.com>
2021-06-14 21:03:51 +0300

    Works
    Need more functions, but works

--- CSharpBasics.project/Program.cs
+++ CSharpBasics.project/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading;
 using System.Transactions;
 
@@ -9,23 +10,30 @@ namespace CSharpBasics.project
     class Program
     {
         static void Main(string[] args)
-
         {
-            //Console based calculator
-            //Calculator should be simple just add/subtract
-            //Possible imputs: 5 + 6, 0 - 2
-            //Output results
-            // The calculator should keep running until 'exit' is typed
-
-            Console.WriteLine("Enter calculation");
-
-            var consoleInput = Console.ReadLine();
             var inputProcessor = new InputProcessor();
             var calculator = new Calculator();
-            InputModel parsedInput = inputProcessor.ProcessInput(consoleInput);
-            var number = calculator.ProcessCalculation(parsedInput);
-            Console.WriteLine(number);
+            string command = "";
+            while (command != "Exit")
+            {
 
+
+                //Console based calculator
+                //Calculator should be simple just add/subtract
+                //Possible imputs: 5 + 6, 0 - 2
+                //Output results
+                // The calculator should keep running until 'exit' is typed
+
+                Console.WriteLine("Enter calculation");
+
+                var consoleInput = Console.ReadLine();
+
+                InputModel parsedInput = inputProcessor.ProcessInput(consoleInput);
+                var number = calculator.ProcessCalculation(parsedInput);
+                Console.WriteLine(number);
+                Console.WriteLine("Type Exit if you want to exit or any button to continue");
+                command = Console.ReadLine();
+            }
         }
 
     }
